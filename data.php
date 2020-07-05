8c3e24bf93dc4938aa2582d393de086e6cb681c9
cosyer <chenyu@mydearest.cn>
2020-07-06 01:33:01 +0800

    first

--- data.php
+++ data.php
@@ -1,5 +1,5 @@
 <?php
-	$conn=mysql_connect("111.231.121.29","root","cy19950604"); //连接数据库地址、用户名、密码
+	$conn=mysql_connect("ip","username","password"); //连接数据库地址、用户名、密码
 	mysql_query("set names 'utf8'"); //数据库编码
 	mysql_select_db("chicken_soup"); //数据库名称
 ?>
